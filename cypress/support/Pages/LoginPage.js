f5b8742cee4ff944284d6ca5fc559ac31c76de34
FlorMarquez1607 <flormarquez1990@gmail.com>
2024-03-13 00:41:19 -0300

    Agregando variables de entorno 1

--- cypress/support/Pages/LoginPage.js
+++ cypress/support/Pages/LoginPage.js
@@ -1,13 +1,13 @@
 export class LoginPage {
 
 
-    Login(User,Pass){
+    Login(){
         cy.request(
             'POST',
             'https://pushing-it.onrender.com/api/login',
             {
-            username: "pushingit",
-            password: "123456!"
+            username: Cypress.env().user,
+            password: Cypress.env().pass
             }).then((response) => {
                 expect(response.status).to.equal(201)
                 window.localStorage.setItem('token',response.body.token)
